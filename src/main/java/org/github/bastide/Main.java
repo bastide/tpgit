de0e838fb78fea96d40df303e122c09842da3b1a
Rémi Bastide <Remi.Bastide@irit.fr>
2024-10-15 16:59:08 +0200

    Rajouté un commentaire

--- src/main/java/org/github/bastide/Main.java
+++ src/main/java/org/github/bastide/Main.java
@@ -5,6 +5,7 @@ package org.github.bastide;
 public class Main {
     public static void main(String[] args) {
         //Un commentaire
+        // Un autre commentaire
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
